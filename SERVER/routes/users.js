7a95957e01d83b333feb3d6a203a797bc1fdbd00
vishal-dev24 <vishalmalviya3846@gmail.com>
2025-12-20 22:48:43 +0530

    Initial commit

---
+++ SERVER/routes/users.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://spancovishal:HzLLrkUjyhrqFz3X@cluster0.bre00kg.mongodb.net/test-fsd')
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log("DB Error:", err);
+    });
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    image: { type: String, default: "" },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
