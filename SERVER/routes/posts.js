7692f726d47371b849b60ad609b6f3a796ed0733
Vishal Malviya <vishalmalviya3846@gmail.com>
2025-12-20 22:44:43 +0530

    Delete SERVER directory

--- SERVER/routes/posts.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const PostSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
-module.exports = mongoose.model('Post', PostSchema);
